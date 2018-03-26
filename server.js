8f12124d3f5c71cd17d980724bf9d7cbab8e2eb9
nyshi <36684541+nyshi@users.noreply.github.com>
2018-03-26 05:54:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1>Doker Swarm node-app version 3</h1> \n');
+  res.send('<h1> welcome CKP Docker Team3</h1> \n');
 });
 
 app.listen(PORT, HOST);
